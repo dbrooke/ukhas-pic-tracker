58f88693357165904a6654309ae8bd3ab33c518c
David Brooke <github@dbrooke.me.uk>
2014-05-21 12:26:27 +0100

    Remove redundant comment and fix whitespace

--- tracker.asm
+++ tracker.asm
@@ -15,19 +15,10 @@
 ;**********************************************************************
 
 
-	list		p=16f690		; list directive to define processor
-	#include	<p16f690.inc>		; processor specific variable definitions
-	
-	__CONFIG    _CP_OFF & _CPD_OFF & _BOR_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_OFF & _FCMEN_OFF & _IESO_OFF
+    list        p=16f690        ; list directive to define processor
+    #include    <p16f690.inc>       ; processor specific variable definitions
 
-
-; '__CONFIG' directive is used to embed configuration data within .asm file.
-; The labels following the directive are located in the respective .inc file.
-; See respective data sheet for additional information on configuration word.
-
-
-
-
+    __CONFIG    _CP_OFF & _CPD_OFF & _BOR_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_OFF & _FCMEN_OFF & _IESO_OFF
 
 
 ;***** VARIABLE DEFINITIONS
@@ -42,33 +33,34 @@ endc
 
 
 ;**********************************************************************
-	ORG		0x000			; processor reset vector
-  	goto		main			; go to beginning of program
+    ORG         0x000           ; processor reset vector
+    goto        main            ; go to beginning of program
 
 
-	ORG		0x004			; interrupt vector location
-	movwf		w_temp			; save off current W register contents
-	movf		STATUS,w		; move status register into W register
-	movwf		status_temp		; save off contents of STATUS register
-	movf		PCLATH,w		; move pclath register into W register
-	movwf		pclath_temp		; save off contents of PCLATH register
+    ORG         0x004           ; interrupt vector location
+    movwf       w_temp          ; save off current W register contents
+    movf        STATUS,w        ; move status register into W register
+    movwf       status_temp     ; save off contents of STATUS register
+    movf        PCLATH,w        ; move pclath register into W register
+    movwf       pclath_temp     ; save off contents of PCLATH register
 
 
 ; isr code can go here or be located as a call subroutine elsewhere
 
-	movf		pclath_temp,w		; retrieve copy of PCLATH register
-	movwf		PCLATH			; restore pre-isr PCLATH register contents	
-	movf		status_temp,w		; retrieve copy of STATUS register
-	movwf		STATUS			; restore pre-isr STATUS register contents
-	swapf		w_temp,f
-	swapf		w_temp,w		; restore pre-isr W register contents
-	retfie					; return from interrupt
+    movf        pclath_temp,w   ; retrieve copy of PCLATH register
+    movwf       PCLATH          ; restore pre-isr PCLATH register contents
+    movf        status_temp,w   ; retrieve copy of STATUS register
+    movwf       STATUS          ; restore pre-isr STATUS register contents
+    swapf       w_temp,f
+    swapf       w_temp,w        ; restore pre-isr W register contents
+    retfie                      ; return from interrupt
 
 
 main
 
 ; remaining code goes here
 
+    goto        $
 
-	END                       ; directive 'end of program'
+    END                       ; directive 'end of program'
 
