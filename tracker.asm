b543c3eb57ba129136c71ea919d7ff2e59cf0f16
David Brooke <github@dbrooke.me.uk>
2014-06-03 12:56:01 +0100

    Added interrupt driven EUSART code from AN774 p16_tiri.asm but not yet initialised. RTTY test message still sent OK.

--- tracker.asm
+++ tracker.asm
@@ -28,39 +28,197 @@
 #define tmr0_reload     d'230'
 #define tmr0_options    b'11010100' ; internal clock source, prescaler 1:32
 
-;***** VARIABLE DEFINITIONS
+RTTY_BUF_LEN    EQU .40 ;length of RTTY buffer
 
-; common locations to all banks 0x70 to 0x7F
+;----------------------------------------------------------------------------
+;Constants
 
-cblock 0x70
-tx_char         ; RTTY transmit character
-bit_count       ; count remaining bits in the transmit character
-tx_ptr          ; pointer to the character in transmit string
-index           ; index for copying strings
-w_temp          ; variable used for context saving
-status_temp     ; variable used for context saving
-pclath_temp     ; variable used for context saving
-endc
+SPBRG_VAL   EQU .25     ;set baud rate 9600 for 4Mhz clock
+TX_BUF_LEN  EQU .80     ;length of transmit circular buffer
+RX_BUF_LEN  EQU TX_BUF_LEN  ;length of receive circular buffer
 
+;----------------------------------------------------------------------------
+;Bit Definitions
 
-;**********************************************************************
-    ORG         0x000           ; processor reset vector
-    goto        main            ; go to beginning of program
+TxBufFull   EQU 0       ;bit indicates Tx buffer is full
+TxBufEmpty  EQU 1       ;bit indicates Tx buffer is empty
+RxBufFull   EQU 2       ;bit indicates Rx buffer is full
+RxBufEmpty  EQU 3       ;bit indicates Rx buffer is empty
+ReceivedCR  EQU 4       ;bit indicates <CR> character received
 
+;----------------------------------------------------------------------------
+;Variables
 
-    ORG         0x004           ; interrupt vector location
-    movwf       w_temp          ; save off current W register contents
-    movf        STATUS,w        ; move status register into W register
-    movwf       status_temp     ; save off contents of STATUS register
-    movf        PCLATH,w        ; move pclath register into W register
-    movwf       pclath_temp     ; save off contents of PCLATH register
+        CBLOCK  0x70
+        WREG_TEMP       ;storage for WREG during interrupt
+        STATUS_TEMP     ;storage for STATUS during interrupt
+        PCLATH_TEMP     ;storage for PCLATH during interrupt
+        FSR_TEMP        ;storage for FSR during interrupt
+        ENDC
 
-    ; is it a timer 0 interrupt ?
-    btfss   INTCON,T0IF
-    goto    isr_end
+        CBLOCK  0x20
+        RTTYBuffer:RTTY_BUF_LEN ;RTTY transmit buffer
+        tx_char         ;RTTY transmit character
+        bit_count       ;count remaining bits in the transmit character
+        tx_ptr          ;pointer to the character in transmit string
+        index           ;index for copying strings
+        Flags           ;byte to store indicator flags
+        TempData        ;temporary data in main routines 
+        BufferData      ;temporary data in buffer routines 
+        TxStartPtr      ;pointer to start of data in TX buffer
+        TxEndPtr        ;pointer to end of data in TX buffer
+        RxStartPtr      ;pointer to start of data in RX buffer
+        RxEndPtr        ;pointer to end of data in RX buffer
+        ENDC
+
+        CBLOCK  0xA0
+        TxBuffer:TX_BUF_LEN ;transmit data buffer
+        ENDC
+
+        CBLOCK  0x120
+        RxBuffer:RX_BUF_LEN ;receive data buffer
+        ENDC
+
+;-----------------------------------------------------------------------------
+;Macros to select the register bank
+; Many bank changes can be optimized when only one STATUS bit changes
+
+Bank0       MACRO           ;macro to select data RAM bank 0
+        bcf STATUS,RP0
+        bcf STATUS,RP1
+        ENDM
+
+Bank1       MACRO           ;macro to select data RAM bank 1
+        bsf STATUS,RP0
+        bcf STATUS,RP1
+        ENDM
+
+Bank2       MACRO           ;macro to select data RAM bank 2
+        bcf STATUS,RP0
+        bsf STATUS,RP1
+        ENDM
+
+Bank3       MACRO           ;macro to select data RAM bank 3
+        bsf STATUS,RP0
+        bsf STATUS,RP1
+        ENDM
+
+;----------------------------------------------------------------------------
+;This code executes when a reset occurs.
+
+        ORG     0x0000      ;place code at reset vector
+
+ResetCode:  clrf    PCLATH      ;select program memory page 0
+        goto    Main        ;go to beginning of program
+
+;----------------------------------------------------------------------------
+;This code executes when an interrupt occurs.
+
+        ORG 0x0004      ;place code at interrupt vector
+
+InterruptCode:  movwf   WREG_TEMP   ;save WREG
+        movf    STATUS,W    ;store STATUS in WREG
+        clrf    STATUS      ;select file register bank0
+        movwf   STATUS_TEMP ;save STATUS value
+        movf    PCLATH,W    ;store PCLATH in WREG
+        movwf   PCLATH_TEMP ;save PCLATH value
+        clrf    PCLATH      ;select program memory page0
+        movf    FSR,W       ;store FSR in WREG
+        movwf   FSR_TEMP    ;save FSR value
+
+        btfsc   INTCON,T0IF ;test timer 0 interrupt
+        goto    isr_RTTY_bit
+
+        Bank0           ;select bank0
+        btfsc   PIR1,RCIF   ;test RCIF receive interrupt
+        bsf STATUS,RP0  ;change to bank1 if RCIF set
+        btfsc   PIE1,RCIE   ;test if interrupt enabled if RCIF set
+        goto    GetData     ;if RCIF and RCIE set, do receive
+
+        Bank0           ;select bank0
+        btfsc   PIR1,TXIF   ;test for TXIF transmit interrupt
+        bsf STATUS,RP0  ;change to bank1 if TXIF set
+        btfsc   PIE1,TXIE   ;test if interrupt enabled if TXIF set
+        goto    PutData     ;if TXIF and TCIE set, do transmit
+
+;can do special error handling here - an unexpected interrupt occurred 
+
+        goto    EndInt
+
+;------------------------------------
+;Get received data and write into receive buffer.
+
+GetData:    Bank0
+        btfsc   RCSTA,OERR  ;test overrun error flag
+        goto    ErrOERR     ;handle it if error
+        btfsc   RCSTA,FERR  ;test framing error flag
+        goto    ErrFERR     ;handle it if error
+
+        btfsc   Flags,RxBufFull ;check if buffer full
+        goto    ErrRxOver   ;handle it if full
+
+        movf    RCREG,W     ;get received data
+        xorlw   0x0d        ;compare with <CR>      
+        btfsc   STATUS,Z    ;check if the same
+        bsf Flags,ReceivedCR ;indicate <CR> character received
+        xorlw   0x0d        ;change back to valid data
+        call    PutRxBuffer ;and put in buffer
+        goto    EndInt
+
+;error because OERR overrun error bit is set
+;can do special error handling here - this code simply clears and continues
+
+ErrOERR:    bcf RCSTA,CREN  ;reset the receiver logic
+        bsf RCSTA,CREN  ;enable reception again
+        goto    EndInt
+
+;error because FERR framing error bit is set
+;can do special error handling here - this code simply clears and continues
+
+ErrFERR:    movf    RCREG,W     ;discard received data that has error
+        goto    EndInt
+
+;error because receive buffer is full and new data has been received
+;can do special error handling here - this code simply clears and continues
+
+ErrRxOver:  movf    RCREG,W     ;discard received data
+        xorlw   0x0d        ;but compare with <CR>      
+        btfsc   STATUS,Z    ;check if the same
+        bsf Flags,ReceivedCR ;indicate <CR> character received
+        goto    EndInt
+
+;------------------------------------
+;Read data from the transmit buffer and transmit the data.
+
+PutData:    Bank0           ;select bank 0
+        btfss   Flags,TxBufEmpty ;check if transmit buffer empty
+        goto    PutDat1     ;if not then go get data and transmit
+        Bank1           ;select bank1
+        bcf PIE1,TXIE   ;disable TX interrupt because all done
+        goto    EndInt
+
+PutDat1:    call    GetTxBuffer ;get data to transmit
+        movwf   TXREG       ;and transmit
+
+;------------------------------------
+;End of interrupt routine restores context
+
+EndInt:     Bank0           ;select bank 0
+        movf    FSR_TEMP,W  ;get saved FSR value
+        movwf   FSR     ;restore FSR
+        movf    PCLATH_TEMP,W   ;get saved PCLATH value
+        movwf   PCLATH      ;restore PCLATH
+        movf    STATUS_TEMP,W   ;get saved STATUS value
+        movwf   STATUS      ;restore STATUS
+        swapf   WREG_TEMP,F ;prepare WREG to be restored
+        swapf   WREG_TEMP,W ;restore WREG without affecting STATUS
+        retfie          ;return from interrupt
+
+;----------------------------------------------------------------------------
+
+isr_RTTY_bit:   Bank0   ;select bank 0
 
     ; reload timer 0
-    banksel TMR0
     movlw   tmr0_reload
     movwf   TMR0
 
@@ -94,7 +252,7 @@ endc
     ; advance pointer for next time
     incf    tx_ptr,f
 
-    goto    isr_end
+    goto    EndInt
 
 null_char
     ; a NULL char terminates the string so disable interrupt
@@ -103,7 +261,7 @@ null_char
     movlw   1
     movwf   bit_count
 
-    goto    isr_end
+    goto    EndInt
 
 next_bit
     ; rotate bit into carry and set port pin accordingly
@@ -113,18 +271,8 @@ next_bit
     btfss   STATUS,C
     bcf     PORTA,5
 
-    goto    isr_end
+    goto    EndInt
 
-; insert next handler here
-
-isr_end
-    movf        pclath_temp,w   ; retrieve copy of PCLATH register
-    movwf       PCLATH          ; restore pre-isr PCLATH register contents
-    movf        status_temp,w   ; retrieve copy of STATUS register
-    movwf       STATUS          ; restore pre-isr STATUS register contents
-    swapf       w_temp,f
-    swapf       w_temp,w        ; restore pre-isr W register contents
-    retfie                      ; return from interrupt
 
 ;**********************************************************************
 ; constant data and tables
@@ -135,7 +283,7 @@ callsign
     dt  "DBTEST",0
 
 ;**********************************************************************
-main
+Main
     ; set I/O pin RA5 (RTTY Tx) as output
     banksel TRISA   ; not bank 0
     bcf     TRISA,5
@@ -154,8 +302,8 @@ main_loop
     btfsc   INTCON,T0IE
     goto    main_loop
 
-    ; start building a test message in RAM at 0xA0
-    movlw   0xA0
+    ; start building a test message in RTTY buffer
+    movlw   RTTYBuffer
     movwf   FSR
 
     ; start with two $ characters
@@ -192,8 +340,8 @@ copied_callsign
     movlw   0x0
     movwf   INDF
 
-    ; set tx pointer to start of message
-    movlw   0xA0
+    ; set tx pointer to start of RTTY buffer
+    movlw   RTTYBuffer
     movwf   tx_ptr
 
     ; reload timer 0
@@ -205,6 +353,193 @@ copied_callsign
     bsf     INTCON,T0IE
 
     goto    main_loop
+
+;----------------------------------------------------------------------------
+;Circular buffer routines.
+
+;----------------------------------------------------------------------------
+;Initialize transmit buffer
+
+InitTxBuffer:   Bank0
+        movlw   LOW TxBuffer    ;take address of transmit buffer
+        movwf   TxStartPtr  ;and place in transmit start pointer
+        movwf   TxEndPtr    ;and place in transmit end pointer
+        bcf Flags,TxBufFull ;indicate Tx buffer is not full
+        bsf Flags,TxBufEmpty ;indicate Tx buffer is empty
+        return
+
+;----------------------------------------------
+;Initialize receive buffer
+
+InitRxBuffer:   Bank0
+        movlw   LOW RxBuffer    ;take address of receive buffer
+        movwf   RxStartPtr  ;and place in receive start pointer
+        movwf   RxEndPtr    ;and place in receive end pointer
+        bcf Flags,RxBufFull ;indicate Rx buffer is not full
+        bsf Flags,RxBufEmpty ;indicate Rx buffer is empty
+        return
+
+;----------------------------------------------
+;Add a byte (from WREG) to the end of the transmit buffer
+
+PutTxBuffer:    Bank1           ;select bank 1
+        bcf PIE1,TXIE   ;disable transmit interrupt
+        Bank0           ;select bank 0
+        btfsc   Flags,TxBufFull ;check if buffer full
+        goto    ErrTxBufFull    ;and go handle error if full
+
+        BANKISEL TxBuffer   ;bank bit for indirect addressing
+        movwf   BufferData  ;save WREG data into BufferData
+        movf    TxEndPtr,W  ;get EndPointer
+        movwf   FSR     ;and place into FSR
+        movf    BufferData,W    ;get BufferData
+        movwf   INDF        ;and store in buffer
+
+;test if buffer pointer needs to wrap around to beginning of buffer memory
+
+        movlw   LOW TxBuffer+TX_BUF_LEN-1 ;get last address of buffer
+        xorwf   TxEndPtr,W  ;and compare with end pointer
+        movlw   LOW TxBuffer    ;load first address of buffer
+        btfss   STATUS,Z    ;check if pointer is at last address
+        incf    TxEndPtr,W  ;if not then increment pointer
+        movwf   TxEndPtr    ;store new end pointer value
+
+;test if buffer is full
+
+        subwf   TxStartPtr,W    ;compare with start pointer
+        btfsc   STATUS,Z    ;check if the same
+        bsf Flags,TxBufFull ;if so then indicate buffer full
+        bcf Flags,TxBufEmpty ;buffer cannot be empty
+        Bank1           ;select bank 1
+        bsf PIE1,TXIE   ;enable transmit interrupt
+        Bank0           ;select bank 0
+        return
+
+;error because attempting to store new data and the buffer is full
+;can do special error handling here - this code simply ignores the byte
+
+ErrTxBufFull:   Bank1           ;select bank 1
+        bsf PIE1,TXIE   ;enable transmit interrupt
+        Bank0           ;select bank 0
+        return          ;no save of data because buffer full
+
+;----------------------------------------------
+;Add a byte (from WREG) to the end of the receive buffer
+
+PutRxBuffer:    Bank0           ;select bank 0
+        btfsc   Flags,RxBufFull ;check if buffer full
+        goto    ErrRxBufFull    ;and go handle error if full
+
+        BANKISEL RxBuffer   ;bank bit for indirect addressing
+        movwf   BufferData  ;save WREG into BufferData
+        movf    RxEndPtr,W  ;get EndPointer
+        movwf   FSR     ;and place into FSR
+        movf    BufferData,W    ;get BufferData
+        movwf   INDF        ;store in buffer
+
+;test if buffer pointer needs to wrap around to beginning of buffer memory
+
+        movlw   LOW RxBuffer+RX_BUF_LEN-1 ;get last address of buffer
+        xorwf   RxEndPtr,W  ;and compare with end pointer
+        movlw   LOW RxBuffer    ;load first address of buffer
+        btfss   STATUS,Z    ;check if pointer is at last address
+        incf    RxEndPtr,W  ;if not then increment pointer
+        movwf   RxEndPtr    ;store new end pointer value
+
+;test if buffer is full
+
+        subwf   RxStartPtr,W    ;compare with start pointer
+        btfsc   STATUS,Z    ;check if the same
+        bsf Flags,RxBufFull ;if so then indicate buffer full
+        bcf Flags,RxBufEmpty ;buffer cannot be empty
+        return
+
+;error because attempting to store new data and the buffer is full
+;can do special error handling here - this code simply ignores the byte
+
+ErrRxBufFull:   return          ;no save of data because buffer full
+
+;----------------------------------------------
+;Remove and return (in WREG) the byte at the start of the transmit buffer
+
+GetTxBuffer:    Bank0           ;select bank 0
+        btfsc   Flags,TxBufEmpty ;check if transmit buffer empty
+        goto    ErrTxBufEmpty   ;and go handle error if empty
+
+        BANKISEL TxBuffer   ;bank bit for indirect addressing
+        movf    TxStartPtr,W    ;get StartPointer
+        movwf   FSR     ;and place into FSR
+
+;test if buffer pointer needs to wrap around to beginning of buffer memory
+
+        movlw   LOW TxBuffer+TX_BUF_LEN-1 ;get last address of buffer
+        xorwf   TxStartPtr,W    ;and compare with start pointer
+        movlw   LOW TxBuffer    ;load first address of buffer
+        btfss   STATUS,Z    ;check if pointer is at last address
+        incf    TxStartPtr,W    ;if not then increment pointer
+        movwf   TxStartPtr  ;store new pointer value
+        bcf Flags,TxBufFull ;buffer cannot be full
+
+;test if buffer is now empty
+
+        xorwf   TxEndPtr,W  ;compare start to end   
+        btfsc   STATUS,Z    ;check if the same
+        bsf Flags,TxBufEmpty ;if same then buffer will be empty
+        movf    INDF,W      ;get data from buffer
+
+        return
+
+;error because attempting to read data from an empty buffer
+;can do special error handling here - this code simply returns zero
+
+ErrTxBufEmpty:  retlw   0       ;tried to read empty buffer
+
+;----------------------------------------------
+;Remove and return (in WREG) the byte at the start of the receive buffer
+
+GetRxBuffer:    Bank0           ;select bank 0
+        btfsc   Flags,RxBufEmpty ;check if receive buffer empty
+        goto    ErrRxBufEmpty   ;and go handle error if empty
+
+        Bank1           ;select bank 1
+        bcf PIE1,RCIE   ;disable receive interrupt
+        Bank0           ;select bank 0
+
+        BANKISEL RxBuffer   ;bank bit for indirect addressing
+        movf    RxStartPtr,W    ;get StartPointer
+        movwf   FSR     ;and place into FSR
+
+;test if buffer pointer needs to wrap around to beginning of buffer memory
+
+        movlw   LOW RxBuffer+RX_BUF_LEN-1 ;get last address of buffer
+        xorwf   RxStartPtr,W    ; and compare with start pointer
+        movlw   LOW RxBuffer    ;load first address of buffer
+        btfss   STATUS,Z    ;check if pointer is at last address
+        incf    RxStartPtr,W    ;if not then increment pointer
+        movwf   RxStartPtr  ;store new pointer value
+        bcf Flags,RxBufFull ;buffer cannot be full
+
+;test if buffer is now empty
+
+        xorwf   RxEndPtr,W  ;compare start to end   
+        btfsc   STATUS,Z    ;check if the same
+        bsf Flags,RxBufEmpty ;if same then buffer will be empty
+        movf    INDF,W      ;get data from buffer
+
+        Bank1           ;select bank 1
+        bsf PIE1,RCIE   ;enable receive interrupt
+        Bank0           ;select bank 0
+        return
+
+;error because attempting to read data from an empty buffer
+;can do special error handling here - this code simply returns zero
+
+ErrRxBufEmpty:  Bank1           ;select bank 1
+        bsf PIE1,RCIE   ;enable receive interrupt
+        Bank0           ;select bank 0
+        retlw   0       ;tried to read empty buffer
+
+;----------------------------------------------------------------------------
 
     END                       ; directive 'end of program'
 
